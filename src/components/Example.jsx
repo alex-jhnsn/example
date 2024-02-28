e1b8bf5d8fb0b8e94d4dcd597621fedd64256f13
Alex Johnson <alex.johnson@myunidays.com>
2024-02-28 18:21:22 +0000

    Add mui

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,3 +1,5 @@
+import { Typography } from "@mui/material";
+
 export default function Example() {
-  return <div>Hello, world!</div>;
+  return <Typography variant="h1">Hello, world!</Typography>;
 }
